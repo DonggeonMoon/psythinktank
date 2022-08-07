31ef296c5db5353b35670b411ca70e7c3ad39d88
DonggeonMoon <nullsafer@naver.com>
2022-08-07 14:55:25 +0900

    fix: 더 이상 사용하지 않는 라이브러리 제거

--- PSYThinktank/src/main/java/com/mdg/PSYThinktank/repository/CircularRepository.java
+++
@@ -1,9 +0,0 @@
-package com.mdg.PSYThinktank.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.mdg.PSYThinktank.model.Circular;
-
-public interface CircularRepository extends JpaRepository<Circular, Integer> {
-
-}
--- PSYThinktank/src/main/java/com/mdg/PSYThinktank/repository/DividendRepository.java
+++
@@ -1,8 +0,0 @@
-package com.mdg.PSYThinktank.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.mdg.PSYThinktank.model.Dividend;
-
-public interface DividendRepository extends JpaRepository<Dividend, String> {
-}
