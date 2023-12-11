fd31e0c703f2368e75d7d040bf3e7d1ca8e7c768
Donggeon Moon <nullsafer@naver.com>
2023-12-11 19:04:40 +0900

    test: 테스트 메서드 작성

--- src/test/java/com/dgmoonlabs/PSYThinktank/domain/board/service/BoardServiceTest.java
+++ src/test/java/com/dgmoonlabs/PSYThinktank/domain/board/service/BoardServiceTest.java
@@ -1,9 +1,9 @@
 package com.dgmoonlabs.PSYThinktank.domain.board.service;
 
+import com.dgmoonlabs.psythinktank.domain.board.dto.BoardResponse;
 import com.dgmoonlabs.psythinktank.domain.board.model.Board;
 import com.dgmoonlabs.psythinktank.domain.board.repository.BoardRepository;
 import com.dgmoonlabs.psythinktank.domain.board.service.BoardService;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -17,15 +17,19 @@ import org.springframework.data.domain.Pageable;
 import java.sql.Timestamp;
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class BoardServiceTest {
 
     private static final PageRequest PAGE_REQUEST = PageRequest.of(1, 10);
+    private static final long boardId1 = 1L;
+    private static final long boardId2 = 2L;
     private static final Board board1 = Board.builder()
             .id(1L)
             .title("제목1")
@@ -44,18 +48,12 @@ class BoardServiceTest {
             .isNotice(false)
             .createdAt(Timestamp.valueOf(LocalDateTime.now()))
             .build();
-    @InjectMocks
-    BoardService boardService;
-    private List<Board> boards;
-    private Page<Board> boardPage;
+    private static final List<Board> boards = List.of(board1, board2);
+    private static final Page<Board> boardPage = new PageImpl<>(boards);
     @Mock
     private BoardRepository boardRepository;
-
-    @BeforeEach
-    void setUp() {
-        boards = List.of(board1, board2);
-        boardPage = new PageImpl<>(boards);
-    }
+    @InjectMocks
+    BoardService boardService;
 
     @Test
     void selectBoards() {
@@ -68,6 +66,11 @@ class BoardServiceTest {
 
     @Test
     void selectBoard() {
+        when(boardRepository.findById(anyLong()))
+                .thenReturn(Optional.of(board1));
+
+        assertThat(boardService.selectBoard(boardId1))
+                .isEqualTo(BoardResponse.from(board1));
     }
 
     @Test
