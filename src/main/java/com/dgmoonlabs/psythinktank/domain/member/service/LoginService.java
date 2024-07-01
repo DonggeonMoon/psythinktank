e501112a2da920bda75060018080f84f38411c33
DonggeonMoon <nullsafer@naver.com>
2024-07-02 03:35:42 +0900

    refactor: 안 쓰는 메서드 삭제

--- src/main/java/com/dgmoonlabs/psythinktank/domain/member/service/LoginService.java
+++ src/main/java/com/dgmoonlabs/psythinktank/domain/member/service/LoginService.java
@@ -1,10 +1,7 @@
 package com.dgmoonlabs.psythinktank.domain.member.service;
 
-import com.dgmoonlabs.psythinktank.domain.member.dto.LoginRequest;
-import com.dgmoonlabs.psythinktank.domain.member.dto.LoginResponse;
 import com.dgmoonlabs.psythinktank.domain.member.model.Member;
 import com.dgmoonlabs.psythinktank.domain.member.repository.MemberRepository;
-import com.dgmoonlabs.psythinktank.global.constant.LoginTry;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.InternalAuthenticationServiceException;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
@@ -12,13 +9,11 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Optional;
 
 import static com.dgmoonlabs.psythinktank.global.constant.LoginResult.*;
 
@@ -26,37 +21,6 @@ import static com.dgmoonlabs.psythinktank.global.constant.LoginResult.*;
 @RequiredArgsConstructor
 public class LoginService implements UserDetailsService {
     private final MemberRepository memberRepository;
-    private final PasswordEncoder passwordEncoder;
-
-    @Transactional(readOnly = true)
-    public LoginResponse login(LoginRequest loginRequest) {
-        if ("".equals(loginRequest.memberId()) || "".equals(loginRequest.memberPw())) {
-            return new LoginResponse(false, BLANK_ID_AND_PASSWORD.getCode(), null);
-        }
-        Optional<Member> memberToCheck = memberRepository.findById(loginRequest.memberId());
-        if (memberToCheck.isEmpty()) {
-            return new LoginResponse(false, ABSENT_ID.getCode(), null);
-        }
-        Member member = memberToCheck.get();
-        if (!LoginTry.includes(member.getLoginTryCount())) {
-            return new LoginResponse(false, LOGIN_TRY_EXCEEDING.getCode(), null);
-        }
-        if (!checkPassword(loginRequest)) {
-            member.increaseLoginTryCount();
-            return new LoginResponse(false, WRONG_PASSWORD.getCode(), member.getLoginTryCount());
-        }
-        member.setLoginTryCount(LoginTry.COUNT_RANGE.getStart());
-        return new LoginResponse(true, SUCCESS.getCode(), member.getLoginTryCount());
-    }
-
-    private boolean checkPassword(LoginRequest loginRequest) {
-        return passwordEncoder.matches(
-                loginRequest.memberPw(),
-                memberRepository.findById(loginRequest.memberId())
-                        .orElseThrow(IllegalStateException::new)
-                        .getPassword()
-        );
-    }
 
     @Transactional
     @Override
