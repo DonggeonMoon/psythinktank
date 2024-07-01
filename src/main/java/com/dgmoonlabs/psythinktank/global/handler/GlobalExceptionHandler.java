5031ba79db4c2ff7faa964980760468d25eadcd7
DonggeonMoon <nullsafer@naver.com>
2024-07-02 03:36:09 +0900

    feat: 예외 응답코드 변경

--- src/main/java/com/dgmoonlabs/psythinktank/global/handler/GlobalExceptionHandler.java
+++ src/main/java/com/dgmoonlabs/psythinktank/global/handler/GlobalExceptionHandler.java
@@ -2,8 +2,10 @@ package com.dgmoonlabs.psythinktank.global.handler;
 
 import com.dgmoonlabs.psythinktank.global.exception.UnauthorizedAccessException;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 import static com.dgmoonlabs.psythinktank.global.constant.ViewName.BOARD_LIST;
 
@@ -11,11 +13,13 @@ import static com.dgmoonlabs.psythinktank.global.constant.ViewName.BOARD_LIST;
 @Slf4j
 public class GlobalExceptionHandler {
     @ExceptionHandler(IllegalArgumentException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
     public void handleArgumentException(IllegalArgumentException exception) {
         log.debug(exception.getMessage());
     }
 
     @ExceptionHandler(UnauthorizedAccessException.class)
+    @ResponseStatus(HttpStatus.UNAUTHORIZED)
     public String handleUnauthorizedAccessException() {
         return BOARD_LIST.redirect();
     }
