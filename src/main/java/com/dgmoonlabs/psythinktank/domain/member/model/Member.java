5ac92924396903ffe48e09e0965db5e4db23ce22
DonggeonMoon <nullsafer@naver.com>
2024-07-05 20:22:51 +0900

    feat: 엔티티 코멘트 추가

--- src/main/java/com/dgmoonlabs/psythinktank/domain/member/model/Member.java
+++ src/main/java/com/dgmoonlabs/psythinktank/domain/member/model/Member.java
@@ -4,6 +4,7 @@ import com.dgmoonlabs.psythinktank.global.constant.LoginTry;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.*;
 import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.Comment;
 import org.hibernate.annotations.CreationTimestamp;
 
 import javax.persistence.*;
@@ -20,29 +21,37 @@ import java.util.Set;
 public class Member implements Serializable {
     @Id
     @Column(name = "member_id", nullable = false, length = 50)
+    @Comment("아이디")
     private String memberId;
 
     @Column(name = "member_pw", nullable = false, length = 100)
+    @Comment("비밀번호")
     private String password;
 
     @Column(name = "member_email", nullable = false, length = 100)
+    @Comment("이메일")
     private String email;
 
     @CreationTimestamp
     @Column(name = "register_date", nullable = false)
+    @Comment("가입일")
     private Date createdAt;
 
     @Column(name = "user_level", nullable = false)
+    @Comment("사용자 등급")
     private int userLevel;
 
     @Column(name = "login_try_count", nullable = false)
     @ColumnDefault(value = "0")
+    @Comment("로그인 시도 횟수")
     private int loginTryCount;
 
     @Column(name = "last_login_datetime")
+    @Comment("마지막 로그인 일시")
     private LocalDateTime lastLoggedInAt;
 
     @Column(name = "last_password_change_datetime")
+    @Comment("마지막 비밀번호 변경일시")
     private LocalDateTime passwordLastChangedAt;
 
     @JsonIgnore
