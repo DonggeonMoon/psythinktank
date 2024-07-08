11b581eb52c449fe65aa430e6507b10e1958af99
DonggeonMoon <nullsafer@naver.com>
2024-07-09 02:08:33 +0900

    fix: 검증 메시지 오류 수정 및 검증 조건 추가

--- src/main/java/com/dgmoonlabs/psythinktank/domain/member/dto/FindIdRequest.java
+++ src/main/java/com/dgmoonlabs/psythinktank/domain/member/dto/FindIdRequest.java
@@ -3,6 +3,6 @@ package com.dgmoonlabs.psythinktank.domain.member.dto;
 import javax.validation.constraints.NotBlank;
 
 public record FindIdRequest(
-        @NotBlank(message = "아이디를 입력해주세요.") String memberEmail
+        @NotBlank(message = "이메일을 입력해주세요.") String memberEmail
 ) {
 }
--- src/main/java/com/dgmoonlabs/psythinktank/domain/member/dto/MemberUserLevelRequest.java
+++ src/main/java/com/dgmoonlabs/psythinktank/domain/member/dto/MemberUserLevelRequest.java
@@ -1,10 +1,12 @@
 package com.dgmoonlabs.psythinktank.domain.member.dto;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 public record MemberUserLevelRequest(
         @NotBlank(message = "아이디를 입력해주세요.")
         String memberId,
+        @Size(message = "사용자 등급이 잘못되었습니다.", min = 1, max = 3)
         int userLevel
 ) {
 }
