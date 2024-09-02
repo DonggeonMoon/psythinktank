4857e07db21bda2de5f9827baa96f4521e0189c3
Donggeon Moon <nullsafer@naver.com>
2024-09-03 04:55:53 +0900

    feat: 국제화 기능 추가

---
+++ src/main/java/com/dgmoonlabs/psythinktank/global/config/WebConfig.java
@@ -0,0 +1,46 @@
+package com.dgmoonlabs.psythinktank.global.config;
+
+
+import com.dgmoonlabs.psythinktank.global.interceptor.I18nInterceptor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+@Configuration
+@RequiredArgsConstructor
+public class WebConfig implements WebMvcConfigurer {
+    private final I18nInterceptor i18nInterceptor;
+
+    @Bean
+    public LocaleResolver localeResolver() {
+        return new SessionLocaleResolver();
+    }
+
+    @Bean
+    public MessageSource messageSource() {
+        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+        messageSource.setBasename("messages");
+        messageSource.setDefaultEncoding("UTF-8");
+        return messageSource;
+    }
+
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+        LocaleChangeInterceptor interceptor = new LocaleChangeInterceptor();
+        interceptor.setIgnoreInvalidLocale(true);
+        return interceptor;
+    }
+
+    @Override
+    public void addInterceptors(final InterceptorRegistry registry) {
+        registry.addInterceptor(i18nInterceptor);
+        registry.addInterceptor(localeChangeInterceptor());
+    }
+}
